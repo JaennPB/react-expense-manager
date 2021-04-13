0f310466c94669413689b80925eddd7878870cdb
jaennpb <jaenn.bucio@gmail.com>
2021-04-13 10:59:58 -0500

    added newexpense comp

--- src/components/Expenses/ExpenseItem/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem/ExpenseItem.js
@@ -1,15 +1,24 @@
+import { useState } from "react";
+
 import ExpenseDate from "./ExpenseItemDate/ExpenseItemDate";
 
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
+  const [title, setTitle] = useState(props.title);
+
+  const changeTitleHandler = () => {
+    setTitle("updated!");
+  };
+
   return (
     <div className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price ">${props.amount}</div>
       </div>
+      <button onClick={changeTitleHandler}>Change Title</button>
     </div>
   );
 };
